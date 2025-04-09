ae76ee6ac6d2a250e3162d7e3e8be317074247ae
Haddel456 <hadeelm046@gmail.com>
2025-04-09 21:55:07 +0300

    Fix shadow color definition in SearchScreen for improved clarity

--- smart_guide/lib/Screens/search_results_screen.dart
+++ smart_guide/lib/Screens/search_results_screen.dart
@@ -44,7 +44,8 @@ class SearchScreenState extends State<SearchScreen> {
               borderRadius: BorderRadius.circular(12),
               boxShadow: [
                 BoxShadow(
-                  color: Colors.black.withOpacity(0.2), // Shadow color
+                  color: Colors.black
+                    ..withAlpha((0.2 * 255).toInt()), // Shadow color
                   spreadRadius: 1, // How much the shadow spreads
                   blurRadius: 5, // How blurry the shadow is
                   offset: Offset(0, 2), // Shadow position (x, y)
